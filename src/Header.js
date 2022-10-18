8151ea10a811f74237b16272db652e358ea5404c
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-10-18 04:27:37 -0700

    react made

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+//imports
+import React from 'react';
+
+
+//class component
+
+class Header extends React.Component {
+  render(){
+    return ( <header></header>
+    )
+  }
+}
+
+//export
+
+export default Header;
