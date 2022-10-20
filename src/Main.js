f980be035afd34aae428139a4648950baa6464ce
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-10-19 22:27:47 -0700

    finished 2 and 3

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,28 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-
-class Main extends React.Component{
-  render(){
+import './Main.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+class Main extends React.Component {
+  render() {
+    let beasts = this.props.data.map((animal, index) => {
+      return <HornedBeast
+        title={animal.title}
+        image_url={animal.image_url}
+        key={index}
+        openModal={this.props.openModal}
+      />
+    });
     return (
       <>
-      <main>
-        <HornedBeast
-         title = "UniWhal" 
-         imgUrl = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jp"
-        description = 'A unicorn and a narwhal nuzzling their horns'
-         />
-        <HornedBeast 
-        title = "Unicorn Head" 
-        imgUrl =  "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-        description = "Someone wearing a very silly unicorn head mask"
-        />
-        <HornedBeast 
-        title = "Rhino Famo" 
-        imgUrl = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-        description = "Parent rhino with two babies"
-        />
-        
-      </main>
+        <main>
+          <Container id='contain' fluid>
+            
+            <Row id='row' xs={1} sm={2} md={3} lg={4} xxl={5} className='g-4'>
+              {beasts}
+            </Row>
+          </Container>
+        </main>
       </>
     )
   }
